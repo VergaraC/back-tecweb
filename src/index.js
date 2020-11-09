bdc00109365e7c19cbb52ecfc3a34d5fb0b95056
Thalia Loiola <thalials@al.insper.edu.br>
2020-11-09 18:25:58 -0300

    tentativa de deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser'); // ler o corpo das requisições
 const morgan = require('morgan'); // logs
 const helmet = require('helmet'); // segurança básica
 const cors = require('cors'); // cross-origin resource sharing google pesquisar
 
-const { PORT } = require('./config/constants');
+// const { PORT } = require('./config/constants');
 
 const app = express();
 
@@ -16,4 +17,4 @@ app.use(morgan('dev'));
 
 require('./app/controllers/')(app);
 
-app.listen(process.env.PORT || PORT);
+app.listen(process.env.PORT || 3333);
